3f4898a5ff321929fb44255f03067a89921064bd
Murilo B. Zague <zaguemurilo7@gmail.com>
2022-12-27 12:53:57 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,22 +3,26 @@ import smtplib
 from email.message import EmailMessage
 from oculto import senha
 
-EMAIL_ADDRESS = 'bote54893@gmail.com'
-EMAIL_PASSWORD = senha
+#DADOS DO EMAIL DE SEU BOT:
+EMAIL_ADDRESS = 'ENDEREÇO DO EMAIL DO BOT'
+EMAIL_PASSWORD = senha  #A SENHA É ARMAZENADA EM 'oculto'
 
+#O CORPO DO EMAIL REMETE A MENSAGEM QUE SERÁ ENVIADA E A FORMATAÇÃO DO TEXTO (BASTA UTILIZAR AS TAGS HTML):
 corpo_email = """
-    <h1>AAAAAAAA</h1>
-    <p><b>UUUUUUUUU</b></p>
-    <p>aaaaaaaaaa</p>
-    """
+    <h1>Título</h1>
+    <p><b>Parágrafo em negrito</b></p>
+    <p>Parágrafo padrão</p>
+"""
 
+#CRIAR O EMAIL QUE SERÁ ENVIADO:
 msg = EmailMessage()
 msg.add_header('Content-Type', 'text/html')
 msg.set_payload(corpo_email )
-msg['Subject'] = 'Teste Message Automatic'
-msg['From'] = 'bote54893@gmail.com'
-msg['To'] = 'murilo.zague@etec.sp.gov.br'
+msg['Subject'] = 'ASSUNTO DO EMAIL'
+msg['From'] = 'SEU EMAIL DE REMETENTE'
+msg['To'] = 'EMAIL DE DESTINATÁRIO'
 
+#CONFIGURAÇÕES E ENVIO DO EMAIL:
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
     smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
     smtp.send_message(msg)
